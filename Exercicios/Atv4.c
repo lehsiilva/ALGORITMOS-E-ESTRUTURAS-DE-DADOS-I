624b133f446ec9ade82adb86f551ea312e2fdc79
lehsiilva <leticiabeatrizlopes@hotmail.com>
2025-02-17 12:17:53 -0300

    Atividades Aula 17/02

---
+++ Exercicios/Atv4.c
@@ -0,0 +1,15 @@
+/*Escreva um programa que calcule o nezemo termo da termo da PA 1,5,9,13 an = a1+(n-1)r */
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+printf("\nDigite o valor de n: ");
+int n;
+scanf("%d", &n);
+int pa = (1 + (n-1)*4); //R é o diferença entre os numeros
+
+printf("\nO numero %dº da PA é: %.2d: ", n, pa);
+
+return 0;
+}
