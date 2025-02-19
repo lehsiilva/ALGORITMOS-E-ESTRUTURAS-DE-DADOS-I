04250d643d041634cf0817b054661b5aadb9fdcc
lehsiilva <leticiabeatrizlopes@hotmail.com>
2025-02-19 09:23:36 -0300

    Exemplo aula 19/02

---
+++ Exercicios/MaiorMenor.c
@@ -0,0 +1,45 @@
+/* Leia 3 números inteiros e selecione o maior e o menor e imprima os seus respectivos valores na tela*/
+
+#include <stdio.h>
+
+int main(){
+    printf("\nDigite 3 valores: ");
+
+    printf("\nDigite o primeiro valor: ");
+    int a;
+    scanf("%d", &a);
+
+    printf("\nDigite o primeiro valor: ");
+    int b;
+    scanf("%d", &b);
+
+    printf("\nDigite o primeiro valor: ");
+    int c;
+    scanf("%d", &c);
+
+    int maior;
+    int menor;
+
+    //Números maiores
+    if(a > b && a > c){
+        maior = a;
+    } else if (b > a && b > c){
+        maior = b;
+    }else{
+        maior = c;
+    }
+
+    //Números menores
+    if(a < b && a < c){
+        menor = a;
+    } else if (b < a && b < c){
+        menor = b;
+    }else{
+        menor = c;
+    }
+
+    printf("\nO maior número é %d ", maior);
+    printf("\nO menor número é %d ", menor);
+
+return 0;
+}
