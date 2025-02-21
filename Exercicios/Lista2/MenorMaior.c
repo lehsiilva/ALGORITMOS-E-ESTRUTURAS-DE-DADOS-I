dcde865e3eb31f92c495c01f8f9e14a3c22d9ae8
lehsiilva <leticiabeatrizlopes@hotmail.com>
2025-02-21 13:12:26 -0300

    Lista 2 Concluída

--- Exercicios/Lista2/MenorMaior.c
+++ Exercicios/Lista2/MenorMaior.c
@@ -3,9 +3,27 @@
 #include <stdio.h>
 
 int main(){
+    printf("\nDígite o primeiro valor: ");
+    double num1;
+    scanf("%lf", &num1);
 
+    printf("\nDígite o segundo valor: ");
+    double num2;
+    scanf("%lf", &num2);
 
+    double maior;
+    double menor;
 
+    if(num1 > num2){
+        maior = num1;
+        menor = num2;
+    } else {
+        maior = num2;
+        menor = num1;
+    }
+
+    printf("\nO maior número é: %.2lf", maior);
+    printf("\nO menor número é: %.2lf", menor);
 
     return 0;
 }
