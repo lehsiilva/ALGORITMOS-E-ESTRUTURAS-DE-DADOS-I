f7f225960c987bd85a23c7de8b2981295daa82dc
lehsiilva <leticiabeatrizlopes@hotmail.com>
2025-03-07 15:39:54 -0300

    Resolução Pendente

--- Exercicios/Lista3 - Estrutura de Repetição/ProvasAlunos.c
+++ Exercicios/Lista3 - Estrutura de Repetição/ProvasAlunos.c
@@ -6,7 +6,54 @@ igual a 80%.*/
 #include <stdio.h>
 
 int main(){
-    
+    printf("\nDigite o numero de alunos:");
+    int numAlunos;
+    scanf("%d",&numAlunos);
+
+    printf("\nDigite o numero de provas:");
+    int numProvas;
+    scanf("%d",&numProvas);
+
+    float mediaAluno;
+
+    float mediaTurma;
+
+    float mediaAlta;
+
+    float SomaNota;
+
+
+    int i = 1;
+
+    while(i <= numAlunos){
+        
+        printf("\nDigite a nota do %d aluno: \n", i);
+        int nota;
+        scanf("%d",&nota);
+
+        SomaNota += nota;
+
+        printf("\nA media do aluno %d é: %.2f\n", i, mediaAluno );
+
+
+        mediaAluno = SomaNota/numProvas;
+        
+
+        mediaTurma = SomaNota/numAlunos;
+
+
+        if(mediaAluno >= 80){
+            mediaAlta++;
+        }
+
+        printf("\nA media da turma é: %.2f", mediaTurma );
+        printf("\nO percentual acima de 80%% é: %.2f", mediaAlta );
+
+        
+
+        i++;
+    }
+
     
 
 
