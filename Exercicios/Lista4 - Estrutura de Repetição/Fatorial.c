11d820234c476289a9e84dce5f9bebaf2c3a5ab5
lehsiilva <leticiabeatrizlopes@hotmail.com>
2025-03-12 11:38:51 -0300

    Concluído

--- Exercicios/Lista4 - Estrutura de Repetição/Fatorial.c
+++ Exercicios/Lista4 - Estrutura de Repetição/Fatorial.c
@@ -1,2 +1,21 @@
 /*Faça um algoritmo que lê um valor N inteiro e positivo e que calcula e escreve o fatorial
 de N (N!)*/
+
+#include <stdio.h>
+
+int main(){
+    printf("\nDigite um numero: ");
+    int num;
+    scanf("%d", &num);
+
+    int fatorial = 1;
+
+    for(int i = num; i > 1; i --){
+        fatorial *= i;
+    }
+
+    printf("\nFatorial de %d e: %d", num, fatorial);
+
+
+
+}
