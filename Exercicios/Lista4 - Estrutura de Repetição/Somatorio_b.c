f3b9b3fe8733369a31bf09f537d924acf9fa0f94
lehsiilva <leticiabeatrizlopes@hotmail.com>
2025-03-13 15:50:01 -0300

    Concluído

--- Exercicios/Lista4 - Estrutura de Repetição/Somatorio_b.c
+++ Exercicios/Lista4 - Estrutura de Repetição/Somatorio_b.c
@@ -4,3 +4,32 @@ um grande número de parcelas. É representado pela letra maiúscula do alfabeto
 i = 1 + 2 + 3 + 4 + 5 = 15
 Crie um programa que calcula o valor da expressão a seguir. Considere que os valores de
 n e m serão fornecidos pelo usuário*/
+
+#include <stdio.h>
+
+int main() {
+
+    printf("\nDigite o valor de N: ");
+    int n;
+    scanf("%d", &n);
+
+    printf("\nDigite o valor de M: ");
+    int m;
+    scanf("%d", &m);
+
+    int somaI = 0;
+    int somaJ;
+
+    for(int i = 1; i <= n; i++){
+        
+        somaJ = 0;
+        for(int j = 1; j <= m; j++){
+            somaJ+=j;
+        } 
+        somaI+=i;
+    }  
+
+    printf("\nResultado: %d", (somaI + somaJ));
+
+
+}
