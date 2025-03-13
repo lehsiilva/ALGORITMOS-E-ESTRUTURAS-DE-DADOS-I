34b38f5ababfa008e273a585c295b0c4cfa67bba
lehsiilva <leticiabeatrizlopes@hotmail.com>
2025-03-12 23:10:26 -0300

    Concluído

--- Exercicios/Lista4 - Estrutura de Repetição/Empresa.c
+++ Exercicios/Lista4 - Estrutura de Repetição/Empresa.c
@@ -3,3 +3,55 @@ programa que leia o preço de um conjunto de n mercadorias, sendo n também um v
 lido. O programa deverá calcular e escrever o maior preço dentre as mercadorias abaixo
 de dois mil reais e o maior preço dentre aquelas a partir de dois mil reais. Rejeitar a
 leitura de valores inválidos*/
+#include <stdio.h>
+
+int main() {
+    
+    int n = 1;
+
+    do{
+
+    printf("\nDigite a quantidade de mercadorias: ");
+    scanf("%d", &n);
+    
+        if(n <= 0){
+            printf("\nInvalido");
+        }
+
+    }while(n <= 0);
+
+    int valor = 1;
+
+    int maiorabaixo = 0;
+    int maioracima = 0;
+
+    int i = 1;
+
+    while (i <= n) {
+
+        do {
+        printf("\nDigite o valor da %d mercadoria: ", i);
+        scanf("%d", &valor);
+
+            if(valor <= 0){
+                printf("\nInvalido");
+
+            }else if (valor < 2000 && valor > maiorabaixo) {
+                maiorabaixo = valor;
+
+            } else if(valor > 2000 && valor > maioracima) {
+                maioracima = valor;
+            }
+
+        }while(valor <= 0);
+
+        i++;
+    }
+
+    printf("\nO produto com o maior preco ABAIXO de 2000R$ e %d",maiorabaixo);
+    printf("\nO produto com o maior preco ACIMA de 2000R$ e %d",maioracima);
+
+    return 0;
+}
+
+
