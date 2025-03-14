bdf308a3695c555d6fc33be8b3e8694fb385cd44
lehsiilva <leticiabeatrizlopes@hotmail.com>
2025-03-13 21:54:20 -0300

    Pendente

--- Exercicios/Lista 5 - Modularização/NumerosPares.c
+++ Exercicios/Lista 5 - Modularização/NumerosPares.c
@@ -1,20 +1,3 @@
 /*Fazer uma função int par(int n) que recebe um número inteiro n e retorna o n-ésimo
 termo da sequência 2, 4, 6, 8, 10, 12, . . */
 
-#include <stdio.h>
-
-int par(int n) {
-   int resul = 2 * n; 
-   return resul; 
-}
-
-int main() {
-    printf("\nDigite qual numero da sequencia voce deseja verificar: ");
-    int n;
-    scanf("%d", &n);
-
-    int resul = par(n);
-    printf("\nO %dº termo da sequência é: %d\n", n, resul);
-
-    return 0;
-}
---
+++ Exercicios/Lista 5 - Modularização/Somatorio.c
@@ -0,0 +1,5 @@
+/*Fazer uma função double somaUmSobreImpar(int n) que recebe um número inteiro n
+e retorna o valor do somatório dos n primeiros termos da sequência anterior. A sua
+função somaUmSobreImpar deve utilizar a função umSobreImpar desenvolvida na questão
+anterior.*/
+
