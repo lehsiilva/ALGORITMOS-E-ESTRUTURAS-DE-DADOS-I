cd9a770e141cf11902c3ff981552189c480c1e4e
lehsiilva <leticiabeatrizlopes@hotmail.com>
2025-03-16 21:43:52 -0300

    Concluído

--- Exercicios/Lista5 - Modularização/NumerosPares.c
+++ Exercicios/Lista5 - Modularização/NumerosPares.c
@@ -1,3 +1,23 @@
 /*Fazer uma função int par(int n) que recebe um número inteiro n e retorna o n-ésimo
 termo da sequência 2, 4, 6, 8, 10, 12, . . */
 
+#include <stdio.h>
+
+int EhPar(int n){
+    
+    int resul = 2 * n;
+
+    return resul;
+}
+
+int main(){
+
+   printf("\nDigite um valor: ");
+   int n;
+   scanf("%d", &n);
+
+   int resul = EhPar(n);
+
+   printf("\nO termo %d da sequencia é: %d", n, resul);
+   return 0;
+}
--- Exercicios/Lista5 - Modularização/OrdemDecrescente.c
+++ Exercicios/Lista5 - Modularização/OrdemDecrescente.c
@@ -3,3 +3,37 @@ inteiro n e mostra na tela (em ordem decrescente) todos os valores menores que n
 a sequência do exercício anterior. A sua função mostrarParesEmOrdemDecrescente deve
 utilizar a função par desenvolvida na questão anterior*/
 
+#include <stdio.h>
+
+int EhPar(int n){
+    
+    int resul = 2 * n;
+
+    return resul;
+}
+
+void mostrarParesEmOrdemDecrescente(int n){
+    
+    int par = EhPar(n);
+
+    for(int i = par; i >= 0; i -= 2 ){
+       
+        printf("\nSequencia: %d ", i);
+    }
+}
+
+
+int main(){
+
+   printf("\nDigite um valor: ");
+   int n;
+   scanf("%d", &n);
+
+   int resul = EhPar(n);
+
+   printf("\nO termo %d da sequencia é: %d", n, resul);
+
+   mostrarParesEmOrdemDecrescente(n);
+
+   return 0;
+}
