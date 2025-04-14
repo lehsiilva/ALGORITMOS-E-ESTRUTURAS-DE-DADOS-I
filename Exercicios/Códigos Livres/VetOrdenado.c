8733910db3dd99e8c252af3d0e6328fd1f91ed7a
lehsiilva <leticiabeatrizlopes@hotmail.com>
2025-04-14 17:29:32 -0300

    Concluído

--- Exercicios/Códigos Livres/VetOrdenado.c
+++ Exercicios/Códigos Livres/VetOrdenado.c
@@ -1,16 +1,51 @@
 /*Implemente uma função que verifique se um vetor está ordenado em ordem crescente.*/
 
 #include <stdio.h>
+#include <stdbool.h>
 
-void vetor(){
+void vetor(int v[], int tam){
     
+    for(int i = 0; i < tam; i++){
+    printf("\nDigite o valor do vetor na posicao %d: ",i);
+    scanf("%d",&v[i]);
+    }
+
 }
 
-void ordenado(){
+void ordenado(int v[], int tam){
+    int troca;
+    bool verifica = true;
+
+    for(int i = 0; i < tam-1; i++){
+        
+        for(int j = 0; j < tam - 1 - i; j++){
+            if(v[j] > v[j+1]){
+                troca = v[j];
+                v[j] = v[j+1];
+                v[j+1] = troca;
+                verifica = false;
+            }
+        }
+    }
+
+    if(verifica == true){
+        printf("\nO vetor esta ordenado de forma crescente");
+    }else{
+        printf("\nO vetor nao esta ordenado de forma crescente");
+    }
 
 }
 
 
 int main(){
+    
+    printf("\nDigite o tamanho do vetor: ");
+    int tam;
+    scanf("%d",&tam);
+
+    int v[tam];
+
+    vetor(v,tam);
+    ordenado(v,tam);
 
 }
