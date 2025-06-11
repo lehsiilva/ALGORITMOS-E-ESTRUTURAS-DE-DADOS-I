87546bc844d3b0fc1d89aadb185d841c79997175
lehsiilva <leticiabeatrizlopes@hotmail.com>
2025-06-10 21:13:30 -0300

    Concluído

---
+++ Exercicios/C++/Exercicio16.cpp
@@ -0,0 +1,42 @@
+/*Crie uma classe Retangulo com:
+Atributos públicos largura e altura (int)
+Um construtor que recebe os dois valores
+Um método calcularArea() que imprime a área (largura × altura)*/
+
+#include <iostream>
+
+using namespace std;
+
+class Retangulo{
+    public:
+        int altura;
+        int largura;
+
+    Retangulo(int a, int l){
+        altura = a;
+        largura = l;
+    }
+
+    void calcularArea(){
+        int area = altura * largura;
+        cout << "Area: " << area << endl;
+    }
+
+};
+
+int main(){
+
+    cout << "Digite a altura: ";
+    int altura;
+    cin >> altura;
+
+    cout << "Digite a largura: ";
+    int largura;
+    cin >> largura;
+
+    Retangulo retangulo(altura,largura);
+
+    retangulo.calcularArea();
+
+    return 0;
+}
